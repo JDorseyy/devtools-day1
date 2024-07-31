5e23c621feda75a09a7cd75899c674cba4cfd74b
Meek Dorsey <meekdorsey@Meeks-MacBook-Air.local>
2024-07-31 15:11:42 -0500

    added a day 2 with task 1-10

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Error 1:
 let message = 'Welcome to the debugging exercise!';
-console.log(mesage); 
+console.log(message); 
 
 // Error 2: 
-let button = document.querySelector('.actionButton'); 
+let button = document.querySelector('#actionButton'); 
 button.addEventListener('click', eventHandler); 
 
 // Error 3: 
@@ -21,9 +21,9 @@ function displayOutput() {
 // Error 6: Misusing variable scope
 function updateMessage() {
   let localMessage = 'Updated message';
+  console.log(localMessage); 
 }
-updateMessage();
-console.log(localMessage); 
+updateMessage(); 
 
 // Error 7:
 document.getElementById('testButton').addEventListener('click', testFunction());
@@ -39,11 +39,11 @@ console.log('Sum is: ' + (number1 - number2));
 
 // Error 9: 
 let nullVar = null;
-console.log(nullVar.length);
+console.log(nullVar);
 
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
@@ -62,7 +62,7 @@ let someNumber = 123;
 console.log(someNumber.toUpperCase()); 
 
 // Error 15: 
-let greeting = Hello, world!;
+let greeting = "Hello, world!";
 
 // Error 16: 
 for (let i = 0; i < 5; i++); {
@@ -70,12 +70,12 @@ for (let i = 0; i < 5; i++); {
 }
 
 // Error 17:
-let five = '5';
+let five = 5;
 if (five === 5) {
     console.log('Five is equal to 5'); // This one should be logged
 } else {
     console.log('Five is not equal to 5'); 
-
+}
 // Error 18:
 Array.prototype.push = function() {
     console.log('Array push method is overwritten');
@@ -90,6 +90,6 @@ x = 5;
 
 // Error 20:
 let myString = 'Hello';
-myString.property = 'World';
-console.log(myString.property); 
+myString.p = 'World';
+console.log(myString + myString.p); 
 
